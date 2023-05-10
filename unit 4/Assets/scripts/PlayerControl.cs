eba24823710493fff4e65a4e75c9c52e3d196b5f
eduardo jc <jibbydotjpeg@gmail.com>
2023-05-10 13:45:52 -0400

    4.3 finished

--- unit 4/Assets/scripts/PlayerControl.cs
+++ unit 4/Assets/scripts/PlayerControl.cs
@@ -7,12 +7,17 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody playerRB;
     public float speed =5.0f;
     private GameObject focalPoint;
+    private bool hasPowerup;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupindicator;
     // Start is called before the first frame update
     void Start()
     {
         
         playerRB = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("FocalPoint");
+        
+
     }
 
     // Update is called once per frame
@@ -20,5 +25,35 @@ public class PlayerControl : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupindicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine( PowerupCountdownRoutine() );
+            powerupindicator.gameObject.SetActive(true);
+
+        }
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupindicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup) 
+        {
+            Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            Debug.Log("collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
+            enemyRigidBody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+
     }
 }
